8a85498cd3bd27b99a98c87d579f0982ee627dc4
zacharyfaulk <zacharyfaulk1@gmail.com>
2017-10-20 22:59:33 -0400

    Original Project - 9/26/2016
    Greedy TSP

---
+++ ZacharyFaulk CECS545 P3/Split.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZacharyFaulk_CECS545_P3
+{
+    class Split
+    {
+        public static void split(newCity[] cityArray, float split, ref float shortSplit, ref float shortDistance, ref List<int> shortList, ref List<int> lastPath)
+        {
+            //Distance variables
+            float distance = 0;
+            float totalDistance = 0;
+
+            //newCorner objects to find the 4 corners
+            newCorner topLeft = new newCorner(float.MaxValue);
+            newCorner topRight = new newCorner(0);
+            newCorner bottomLeft = new newCorner(float.MaxValue);
+            newCorner bottomRight = new newCorner(0);
+
+            //Lists to store the cities above and below the split point
+            List<int> topList = new List<int>();
+            List<int> topFinal = new List<int>();
+            List<int> bottomList = new List<int>();
+            List<int> bottomFinal = new List<int>();
+
+            //Check each city in the cityArray to see if it's
+            //above or below the split point
+            for (int c = 1; c < cityArray.Length + 1; c++)
+            {
+                //Above the split point
+                if (cityArray[c - 1].yCoordinate > split)
+                {
+                    topList.Add(c);     //Add to the topList
+
+                    //Check if the city is the left or right most city
+                    if (cityArray[c - 1].xCoordinate < topLeft.coordinate)
+                    {
+                        topLeft.id = c;
+                        topLeft.coordinate = cityArray[c - 1].xCoordinate;
+                    }
+                    if (cityArray[c - 1].xCoordinate > topRight.coordinate)
+                    {
+                        topRight.id = c;
+                        topRight.coordinate = cityArray[c - 1].xCoordinate;
+                    }
+                }
+
+                //At or below the split point
+                else
+                {
+                    bottomList.Add(c);  //Add to the bottom list
+
+                    //Check if the city is the left or right most city
+                    if (cityArray[c - 1].xCoordinate < bottomLeft.coordinate)
+                    {
+                        bottomLeft.id = c;
+                        bottomLeft.coordinate = cityArray[c - 1].xCoordinate;
+                    }
+                    if (cityArray[c - 1].xCoordinate > bottomRight.coordinate)
+                    {
+                        bottomRight.id = c;
+                        bottomRight.coordinate = cityArray[c - 1].xCoordinate;
+                    }
+                }
+            }
+
+            //Adding the corner cities to the top/bottomFinal lists
+            //Removing the corner cities from the top/bottom lists
+            topFinal.Add(topLeft.id);
+            topFinal.Add(topRight.id);
+            bottomFinal.Add(bottomRight.id);
+            bottomFinal.Add(bottomLeft.id);
+            topList.Remove(topLeft.id);
+            topList.Remove(topRight.id);
+            bottomList.Remove(bottomLeft.id);
+            bottomList.Remove(bottomRight.id);
+
+            //Call insertNode function to generate the new greedy paths
+            InsertNode.insertNode(cityArray, topList, ref topFinal);
+            InsertNode.insertNode(cityArray, bottomList, ref bottomFinal);
+
+            //Combine the top and bottom paths to complete the tour
+            topFinal.AddRange(bottomFinal);
+            topFinal.Add(topFinal[0]);
+
+            //If the path's distance wasn't the most recent path to be computed
+            //find the distance of the path
+            if (topFinal.SequenceEqual(lastPath) == false)
+            {
+                for (int d = 1; d < topFinal.Count; d++)
+                {
+                    int xy1 = topFinal[d - 1] - 1; //Location of city A in city List
+                    int xy2 = topFinal[d] - 1;     //Location of city B in city List
+
+                    //Find x and y coordinates of city A and B
+                    float x1 = cityArray[xy1].xCoordinate;
+                    float x2 = cityArray[xy2].xCoordinate;
+                    float y1 = cityArray[xy1].yCoordinate;
+                    float y2 = cityArray[xy2].yCoordinate;
+
+                    //Use distance equation to find the distance between city A and B
+                    //Add distance to totalDistance
+                    distance = (float)Math.Sqrt((((x2 - x1)) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+                    totalDistance = totalDistance + distance;
+                }
+
+                //If the new path distance is smaller than the
+                //current smallest distance then replace
+                //the smallest distance with the new distance
+                if (totalDistance < shortDistance)
+                {
+                    shortDistance = totalDistance;  //Store the new shorter distance
+                    shortList = topFinal;           //Store the corresponding path
+                    shortSplit = split;             //Store the current split value
+                }
+
+                //Store the most recent path to avoid
+                //computing the same path multiple times
+                lastPath = topFinal;
+            }
+
+        }
+    }
+}
---
+++ ZacharyFaulk CECS545 P3/ZacharyFaulk CECS545 P3.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ZacharyFaulk_CECS545_P3
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            //Code needed to run the Windows Form Application
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            string fileName = "Random40.TSP";    //File name being tested
+
+            //Variables to keep track of execution time
+            Stopwatch stopWatch = new Stopwatch();
+            float time1 = 0;
+            string time2 = null;
+            string timer = null;
+            stopWatch.Start();      //Start Stopwatch
+
+            int count = 0;
+            float split = 2;        //Starting split point
+            float shortSplit = 0;   //Split point resulting in shorest path
+            float shortDistance = float.MaxValue;   //Stores shortest distance
+            List<int> shortList = new List<int>();  //Stores shorest path
+            List<int> lastPath = new List<int>();   //Stores the last tested path
+
+            //Reads file to find # of lines
+            //Stores (# of lines  - 7) to get the number of cities in the file
+            string[] lines = File.ReadAllLines(fileName);
+            int newLength = lines.Length - 7;
+
+            //Array of newCity objects with size = # of cities
+            newCity[] cityArray = new newCity[newLength];   
+
+            //Stores city data
+            string line;
+            System.IO.StreamReader file = new System.IO.StreamReader(fileName);
+            while ((line = file.ReadLine()) != null)
+            {
+                //Ignore first 7 lines of the file
+                //8th line is where the city data begins
+                if (count >= 7)
+                {
+                    string[] data = line.Split(' ');    //Split and store the city data in data string array
+
+                    //Create newCity object using data pulled from data array (ID, x Coordinate, y Coordinate)
+                    //Store each newCity object in newCity array
+                    cityArray[count - 7] = new newCity(Int32.Parse(data[0]), float.Parse(data[1]), float.Parse(data[2]));
+                }
+                count++;
+            }
+            //Close File
+            file.Close();
+
+            //Split the map of cities at y = split to find
+            //the split point that results in the shorest path
+            for (split = 2; split < 100; split++)
+            {
+                //Call the split function to split the map of cities at y = split
+                Split.split(cityArray, split, ref shortSplit, ref shortDistance, ref shortList, ref lastPath);
+            }
+
+            stopWatch.Stop();   //Stop Stopwatch
+
+            //Print distance/path/split/time data
+            Console.WriteLine("File = " + fileName);
+            Console.WriteLine("The Greedy Distance is " + shortDistance);
+            Console.WriteLine("The Greedy Path is " + string.Join(",", shortList));
+            Console.WriteLine("Shortest split at y = " + shortSplit);
+            time1 = stopWatch.ElapsedMilliseconds;
+            time1 = (float)TimeSpan.FromMilliseconds(time1).TotalSeconds;
+            time2 = stopWatch.ElapsedMilliseconds.ToString();
+            timer = time1.ToString();
+            Console.WriteLine("Execution time took " + time2 + " Milliseconds, or " + timer + " Seconds");
+
+            //Run Windows Form Application to graph the shortest path
+            Application.Run(new P3_GUI(cityArray, shortList));
+
+            Console.ReadKey();
+        }
+    }
+
+    //newCity class that creates newCity objects
+    //Each object has an ID, x coordinate, y coordniate
+    public class newCity
+    {
+        public int id;
+        public float xCoordinate;
+        public float yCoordinate;
+            
+        public newCity(int id, float x, float y)
+        {
+            this.id = id;
+            this.xCoordinate = x;
+            this.yCoordinate = y;
+         }
+    }
+
+    //newCorner class that creates newCorner objects
+    //Each object has an ID, tempID, coordinate float
+    //Used to find the left and right most cities on each side of the split map
+    public class newCorner
+    {
+        public int id;
+        public int tempID;
+        public float coordinate;
+
+        public newCorner(float x)
+        {
+            this.coordinate = x;
+        }
+    }
+}
+
